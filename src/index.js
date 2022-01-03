b137a48d4392fe348601227690b54e7b42775569
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-01-03 13:44:10 -0800

    Converting to React component form (w/ functions)

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reactlogo from "./react-logo.png";
 
-const page = (
-  <div>
-    <img src={reactlogo} width="40px" alt="" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013 </li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function Page() {
+  return (
+    <div>
+      <img src={reactlogo} width="40px" alt="" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013 </li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
