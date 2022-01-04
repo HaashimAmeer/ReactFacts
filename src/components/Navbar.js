2d4a6e7eeacc698830fcdb9de5b672254831eb81
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-01-03 21:55:06 -0800

    Added background image to Main component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import reactlogo from "../react-logo.png";
+import reactlogo from "../images/react-logo.png";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <img className="react-img" src={reactlogo} width="40px" alt="" />
-      <h3 className="site-name">ReactFacts</h3>
-      <h4 className="description">React Course - Project 1</h4>
+      <img className="nav-logo" src={reactlogo} width="40px" alt="" />
+      <h3 className="nav-title">ReactFacts</h3>
+      <h4 className="nav-description">React Course - Project 1</h4>
     </nav>
   );
 }
